056381f16c820616730e9eaaf9474bbdcb094889
吴均均 <2496235906@qq.com>
2018-12-22 18:59:44 +0800

    把本地工程添加到git

---
+++ git-test/src/cn/itcast/test/GitTest.java
@@ -0,0 +1,14 @@
+package cn.itcast.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git....");
+        System.out.println("Hello Git....");
+        System.out.println("Hello Git....");
+        System.out.println("Hello Git....");
+        System.out.println("Hello Git....");
+        System.out.println("Hello Git....");
+        System.out.println("Hello Git....");
+    }
+}
